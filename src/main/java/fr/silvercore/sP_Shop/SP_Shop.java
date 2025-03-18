8d4be606a8f8e483601117710db649bc16206b43
Jemy5 <jeremy.etuy@proton.me>
2025-03-18 09:09:55 +0100

    SP_Shop

--- src/main/java/fr/silvercore/sP_Shop/SP_Shop.java
+++ src/main/java/fr/silvercore/sP_Shop/SP_Shop.java
@@ -4,40 +4,34 @@
  * @author Silverdium
  * @author Netsu_ - main
  * @author JemY5
- * 
- * 
- * 
- * 
- * 
- * 
  */
 package fr.silvercore.sP_Shop;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.silvercore.sP_Shop.commands.CommandSell;
-import fr.silvercore.sP_Shop.commands.CommandShop;
+//import fr.silvercore.sP_Shop.commands.CommandSell;
+//import fr.silvercore.sP_Shop.commands.CommandShop;
 
 public class SP_Shop extends JavaPlugin {
 
-    static SP_Shop instance;
+   //   static SP_Shop instance;
 
     @Override
     public void onEnable() {
-
+        Bukkit.getLogger().log(level.INFO, "SilverPlugins : SP_Shop a démarré avec succès (le frère de réussite).");
         instance = this;
 
-        getCommand("shop").setExecutor(new CommandShop());
-        getCommand("sell").setExecutor(new CommandSell());
+        //getCommand("shop").setExecutor(new CommandShop());
+        //getCommand("sell").setExecutor(new CommandSell());
 
     }
 
     @Override
     public void onDisable() {
-
+        Bukkit.getLogger().log(level.INFO, "SilverPlugins : SP_Shop a désactivé avec réussite (le frère de succès).");
     }
 
-    public static SP_Shop getInstance() {
-        return instance;
-    }
+    //public static SP_Shop getInstance() {return instance;
+    //}
 }
