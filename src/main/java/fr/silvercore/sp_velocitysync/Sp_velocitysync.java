b0e4f2e605fb34f32492853214343f76fadfdd48
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-03-29 09:51:55 +0100

    create first test structure & comment it

--- src/main/java/fr/silvercore/sp_velocitysync/Sp_velocitysync.java
+++ src/main/java/fr/silvercore/sp_velocitysync/Sp_velocitysync.java
@@ -6,6 +6,16 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import org.slf4j.Logger;
 
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import com.velocitypowered.api.proxy.server.ServerPing;
+import com.velocitypowered.api.proxy.server.ServerInfo;
+import com.velocitypowered.api.proxy.Player;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Optional;
+
+
 @Plugin(
         id = "sp_velocitysync",
         name = "sp_velocitysync",
@@ -16,7 +26,43 @@ public class Sp_velocitysync {
     @Inject
     private Logger logger;
 
+    @Inject
+    private ProxyServer proxy;
+
     @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
+    public void onProxyInitialization(ProxyInitializeEvent event) { // function éxécuter lors du lancement du proxy
+
+        proxy.getScheduler().buildTask(this, this::logServerAndPlayerInfo) // créer la tache : logServerAndPlayerInfo
+
+                .repeat(10, TimeUnit.SECONDS) // la tache doit s'éxécuter toute les 10 en seconde
+                .schedule();  // lance la planification de la tache
+
+    }
+
+
+    private void logServerAndPlayerInfo() {  // function appeler par onProxyInitialization
+
+        logger.info("=== Serveurs enregistrés ==="); // "console.log"
+
+        for (RegisteredServer server : proxy.getAllServers()) { // récupération des servers
+
+            ServerInfo info = server.getServerInfo(); // récupération des info des servers
+            Optional<ServerPing> ping = server.ping().join(); // récupération du ping des servers / .join sert a attendre la fin de la req async
+
+            logger.info("Nom: " + info.getName()); // log server info ...
+            logger.info("Adresse: " + info.getAddress()); // ...
+            logger.info("Joueurs: " + (ping.isPresent() ? ping.get().getPlayers().orElse(null) : "Inconnu")); // ...
+
+        }
+
+        logger.info("=== Joueurs connectés ==="); // "console.log"
+
+        for (Player player : proxy.getAllPlayers()) { // récupération des info des joueurs de tout les server
+
+            logger.info("Pseudo: " + player.getUsername()); // log players info
+            logger.info("UUID: " + player.getUniqueId()); // ...
+            logger.info("Serveur actuel: " + (player.getCurrentServer().isPresent() ? player.getCurrentServer().get().getServerInfo().getName() : "Aucun")); // ...
+
+        }
     }
 }
